704a709158393d59d2854906740f5f97604d9236
Your Name <dfinster@vultr.com>
2021-01-06 15:32:58 -0500

    echo md5sum to log

--- Marketplace/scripts/install-cloud-init.sh
+++ Marketplace/scripts/install-cloud-init.sh
@@ -2,7 +2,7 @@
 
 cd /tmp
 wget https://ewr1.vultrobjects.com/cloud_init_beta/cloud-init_ubuntu_latest.deb
-md5sum cloud-init_ubuntu_latest.deb > /tmp/cloud-init_ubuntu_latest.md5sum
+md5sum cloud-init_ubuntu_latest.deb
 
 apt-get -y install python3-jinja2
 apt-get -y install python3-jsonpatch
