4e12327d6d1ab5bdf89c4672d92a50233fc207d6
Eric Benner <ebenner@vultr.com>
2021-01-08 14:10:42 -0500

    Fix issues and use universal deb

--- Marketplace/scripts/install-cloud-init.sh
+++ Marketplace/scripts/install-cloud-init.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 cd /tmp
-wget https://ewr1.vultrobjects.com/cloud_init_beta/cloud-init_ubuntu_latest.deb
-md5sum cloud-init_ubuntu_latest.deb
+wget https://ewr1.vultrobjects.com/cloud_init_beta/cloud-init_universal_latest.deb
+md5sum cloud-init_universal_latest.deb
 
-apt-get -y install python3-jinja2
-apt-get -y install python3-jsonpatch
-apt-get -y install python3-jsonschema
-dpkg -i cloud-init_ubuntu_latest.deb
+dpkg -i cloud-init_universal_latest.deb
 apt-get install -f -y
 
-systemctl start cloud-init
 sleep 5
 
+mkdir -p /var/lib/cloud/scripts/per-instance/
 mv /tmp/provision.sh /var/lib/cloud/scripts/per-instance/provision.sh
 chmod +x /var/lib/cloud/scripts/per-instance/provision.sh
