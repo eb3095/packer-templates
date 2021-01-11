4f028bcd85f98e1666dadca46da9be6046bd7f8c
David Finster <dfinster@vultr.com>
2021-01-11 16:24:33 -0500

    Added apt update per EB.

--- Marketplace/scripts/install-cloud-init.sh
+++ Marketplace/scripts/install-cloud-init.sh
@@ -4,6 +4,8 @@ cd /tmp
 wget https://ewr1.vultrobjects.com/cloud_init_beta/cloud-init_universal_latest.deb
 md5sum cloud-init_universal_latest.deb
 
+apt update -y
+
 dpkg -i cloud-init_universal_latest.deb
 apt-get install -f -y
 
